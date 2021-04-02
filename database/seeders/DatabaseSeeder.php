d24f1920452490852f837ebcd7b117c40e34bdf2
Honore <gnamou@gnamou.com>
2021-04-02 11:51:10 +0000

    troisieme commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
-        \App\Models\produit::factory(100)->create();
-        \App\Models\ministere::factory(100)->create();
+       //  \App\Models\User::factory(10)->create();
+      //  \App\Models\produit::factory(100)->create();
+      //  \App\Models\ministere::factory(100)->create();
     }
 }
